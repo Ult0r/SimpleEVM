04623c5d9d71f3cc07df7dd52ac0051433a7d51e
Ult0r <lreimers@itemis.de>
2017-06-19 14:42:01 +0200

    added evmwordtests to suite

--- src/org/itemis/test/AllTests.java
+++ src/org/itemis/test/AllTests.java
@@ -1,6 +1,7 @@
 package org.itemis.test;
 
 import org.itemis.test.evm.UtilsTest;
+import org.itemis.test.evm.types.EVMWordTest;
 import org.itemis.test.evm.types.UnsignedByteTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,7 +10,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
   UnsignedByteTest.class,
-  UtilsTest.class
+  UtilsTest.class,
+  EVMWordTest.class
 })
 public class AllTests {
 
